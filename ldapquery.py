5250e29b628f501832e5904e6655ab9a0c5e3cd1
scott <flyincloud@gmail.com>
2024-02-04 10:48:50 +0800

    add password auth

--- ldapquery.py
+++ ldapquery.py
@@ -1,9 +1,9 @@
-from ldap3 import Server, Connection, ALL
+from ldap3 import Server, Connection, ALL, MODIFY_REPLACE, MODIFY_ADD, MODIFY_DELETE
 from flask import request
 from flask import jsonify
 from flask import Flask
 from flask import json
-
+import traceback
 import os
 
 
@@ -56,22 +56,56 @@ def lookup():
 
      # Set up the server connection
     server = Server(LDAP_SERVER, get_info=ALL)
-    conn = Connection(server, user=LDAP_USER, password=LDAP_PASS)
-    conn.open()
-    conn.bind()
-    # Perform the search
-    print('pppp' + filter)
-    conn.search(LDAP_BASE, filter, attributes=attributes, size_limit=SEARCH_LIMIT)
+    with Connection(server, user=LDAP_USER, password=LDAP_PASS) as conn:
+        conn.open()
+        conn.bind()
+        # Perform the search
+        #print('pppp' + filter)
+        conn.search(LDAP_BASE, filter, attributes=attributes, size_limit=SEARCH_LIMIT)
 
-    result = []
-    for entry in conn.entries:
-        vals = entry.entry_attributes_as_dict
-        result.append(vals)
+        result = []
+        for entry in conn.entries:
+            vals = entry.entry_attributes_as_dict
+            result.append(vals)
 
-    if result:
-        return jsonify(result), 200, {'Content-Type': 'application/json;charset=UTF-8'}
-    else:
-        return jsonify({'error': 'account is not found'}), 404, {'Content-Type': 'application/json;charset=UTF-8'}
+        if result:
+            return jsonify(result), 200, {'Content-Type': 'application/json;charset=UTF-8'}
+        else:
+            return jsonify({'error': 'account is not found'}), 404, {'Content-Type': 'application/json;charset=UTF-8'}
+
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    username = request.form['username']
+    password = request.form['password']
+    server = Server(LDAP_SERVER, get_info=ALL)
+    try:
+        with Connection(server, user=f'tonghao\\{username}', password=password) as conn:
+            return jsonify({'auth': 'success'}), 200, {'Content-Type': 'application/json;charset=UTF-8'}
+    except Exception as err:
+        traceback.print_exc()
+        return jsonify({'auth': 'failed'}), 400, {'Content-Type': 'application/json;charset=UTF-8'}
+
+
+@app.route("/changepw", methods=["POST"])
+def changepw():
+    ''' Not working. Maybe it must be LDAPS'''
+    username = request.form['username']
+    password = request.form['old_password']
+    new_pass = request.form['new_password']
+    server = Server(LDAP_SERVER, get_info=ALL)
+    try:
+        with Connection(server, user=f'tonghao\\{username}', password=password) as conn:
+            # oldpwd_utf16 = '"{0}"'.format(password).encode('utf-16-le')
+            # newpwd_utf16 = '"{0}"'.format(new_pass).encode('utf-16-le')
+            # conn.modify('CN=戴智杰,OU=Employee,DC=Tonghao,DC=local',
+            #             {'unicodePwd': [(MODIFY_DELETE, [oldpwd_utf16]), (MODIFY_ADD, [newpwd_utf16])]})
+            conn.extend.microsoft.modify_password('CN=戴智杰,OU=Employee,DC=Tonghao,DC=local', new_password=new_pass, old_password=password)
+            print(conn.result)
+            return jsonify({'operation': 'success'}), 200, {'Content-Type': 'application/json;charset=UTF-8'}
+    except Exception as err:
+        traceback.print_exc()
+        return jsonify({'operation': 'failed'}), 400, {'Content-Type': 'application/json;charset=UTF-8'}
 
 
 def main():
